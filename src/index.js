273b52449c4b660156ba964d512c1913a87f8640
ahmedraza13 <ahmedfaisalakhawala@gmail.com>
2023-07-15 17:26:55 +0500

    practice 1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.Fragment>
+  <h1>Thapa Technical Netflix Pick</h1>
+  <p>List of 5 Best Series</p>
+  <ol>
+    <li>Dark</li>
+    <li>Extra curricular</li>
+    <li>My Holo love</li>
+    <li>My Holo</li>
+    <li>Champ</li>
+  </ol>
+  </React.Fragment>
+)
